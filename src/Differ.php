c29ca675facafd7ce29eb7e58d453272ae943aa7
k.kuzmin <k.kuzmin@smileexpo.ru>
2019-03-21 16:57:51 +0300

    Data to Json

--- src/Differ.php
+++ src/Differ.php
@@ -4,19 +4,13 @@ namespace Godedok;
 
 use Funct\Collection;
 
-function genDiff($json1, $json2)
+function genDiff($data1, $data2)
 {
-    $arr1 = json_decode($json1, true);
-    $arr2 = json_decode($json2, true);
-    $array1 = array_map(function ($key) use ($arr1) {
-        return $key . "0" . ":" . $arr1[$key];
-    }, array_keys($arr1));
-    $array2 = array_map(function ($key) use ($arr2) {
-        return $key . "1" . ":" . $arr2[$key];
-    }, array_keys($arr2));
-    $result = array_merge($array1, $array2);
-    $res = Collection\groupBy($result, function ($item) {
-        return mb_substr(explode(':', $item)[0], 0, -1);
-    });
-    return $res;
+    return '';
+}
+
+function getData(string $pathToFile): array
+{
+    $content = file_get_contents($pathToFile);
+    return json_decode($content, true);
 }
--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -4,39 +4,26 @@ namespace Godedok\Tests;
 
 use \PHPUnit\Framework\TestCase;
 use function Godedok\genDiff;
+use function Godedok\getData;
 
 class DifferTest extends TestCase
 {
     /**
      * @dataProvider additionProvider
      */
-    public function testGenDiff($expected, $json1, $json2)
+    public function testGenDiff($expected, $beforeData, $afterData)
     {
-        $this->assertEquals($expected, genDiff($json1, $json2));
+        $this->assertEquals($expected, genDiff($beforeData, $afterData));
     }
 
     public function additionProvider()
     {
+        $expected = file_get_contents('tests/data/diffJson');
+        $beforeData = getData('tests/data/before.json');
+        $afterData = getData('tests/data/after.json');
+
         return [
-            [
-                [
-                    "host" => "hexlet.io",
-                    "timeout1" => 20,
-                    "timeout0" => 50,
-                    "proxy0" => "123.234.53.22",
-                    "verbose1" => true
-                ],
-                '{
-                    "host": "hexlet.io",
-                    "timeout": 50,
-                    "proxy": "123.234.53.22"
-                }',
-                '{
-                    "timeout": 20,
-                    "verbose": true,
-                    "host": "hexlet.io"
-                }'
-            ],
+            [$expected, $beforeData, $afterData],
         ];
     }
 }
