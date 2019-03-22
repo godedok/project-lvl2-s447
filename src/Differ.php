01b677356a65323a209ff692a8186dd61f753a35
Konstantin <gdededok@gmail.com>
2019-03-22 07:28:45 +0300

    Add logic

--- src/Differ.php
+++ src/Differ.php
@@ -2,11 +2,38 @@
 
 namespace Godedok;
 
-use Funct\Collection;
-
 function genDiff($data1, $data2)
 {
-    return '';
+    $diff = calcDiff($data1, $data2);
+    $result = '{' . PHP_EOL;
+        foreach ($diff as $key => $value) {
+            $result .= '  ' . $key . ': ' . json_encode($value) . PHP_EOL;
+        }
+        $result .= '}' . PHP_EOL;
+    return $result;
+}
+
+function calcDiff(array $data1, array $data2): array
+{
+    $result = [];
+    foreach ($data1 as $key => $value) {
+        if (array_key_exists($key, $data2)) {
+            if ($value === $data2[$key]) {
+                $result['  ' . $key] = $value;
+            } else {
+                $result['- ' . $key] = $data1[$key];
+                $result['+ ' . $key] = $data2[$key];
+            }
+        } else {
+            $result['- ' . $key] = $value;
+        }
+    }
+    foreach ($data2 as $key => $value) {
+        if (!array_key_exists($key, $data1)) {
+            $result['+ ' . $key] = $value;
+        }
+    }
+    return $result;
 }
 
 function getData(string $pathToFile): array
